a23d6fa5bcd48ae3e8de5522ba035deaab7c3eb1
cao-qh <caoqihao88@gmail.com>
2024-03-01 14:14:18 +0800

    统一管理api案例完成

--- src/Test.vue
+++ src/Test.vue
@@ -1,23 +1,5 @@
 <template>
-  <div>
-    <h1>测试axios二次封装</h1>
-  </div>
+  <div>App根组件</div>
 </template>
 
-<script setup lang="ts">
-import request from '@/utils/request'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  request({
-    url: '/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '111111',
-    },
-  }).then((res) => {
-    console.log(res)
-  })
-})
-</script>
+<script setup lang="ts"></script>
