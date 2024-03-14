ad4b267a6422fd64a5bc3f4f8c7589072cd71d8c
cao-qh <caoqihao88@gmail.com>
2024-03-14 18:24:47 +0800

    编写上传图片组件

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,7 @@ declare module 'vue' {
     ABreadcrumb: typeof import('ant-design-vue/es')['Breadcrumb']
     AButton: typeof import('ant-design-vue/es')['Button']
     ACard: typeof import('ant-design-vue/es')['Card']
+    ACheckbox: typeof import('ant-design-vue/es')['Checkbox']
     ACol: typeof import('ant-design-vue/es')['Col']
     AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
     ADropdown: typeof import('ant-design-vue/es')['Dropdown']
@@ -17,6 +18,7 @@ declare module 'vue' {
     AForm: typeof import('ant-design-vue/es')['Form']
     AFormItem: typeof import('ant-design-vue/es')['FormItem']
     AInput: typeof import('ant-design-vue/es')['Input']
+    AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
     ALayout: typeof import('ant-design-vue/es')['Layout']
     ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']
     ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader']
@@ -30,9 +32,11 @@ declare module 'vue' {
     ASpace: typeof import('ant-design-vue/es')['Space']
     ATable: typeof import('ant-design-vue/es')['Table']
     ATextarea: typeof import('ant-design-vue/es')['Textarea']
+    AUpload: typeof import('ant-design-vue/es')['Upload']
     PageWrapper: typeof import('./src/components/PageWrapper/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SvgIcon: typeof import('./src/components/SvgIcon/index.vue')['default']
+    UploadImage: typeof import('./src/components/UploadImage/index.vue')['default']
   }
 }
--- src/views/product/trademark/modules/Add.vue
+++ src/views/product/trademark/modules/Add.vue
@@ -5,72 +5,38 @@
     @ok="$emit('onOk')"
     @cancel="$emit('onCancel')"
   >
-    <a-form
-      ref="formRef"
-      :model="formState"
-      layout="vertical"
-      name="form_in_modal"
-    >
-      <a-form-item
-        name="title"
-        label="Title"
-        :rules="[
-          {
-            required: true,
-            message: 'Please input the title of collection!',
-          },
-        ]"
-      >
-        <a-input v-model:value="formState.title" />
+    <a-form :mode="formState">
+      <a-form-item label="品牌名称" name="trademarkName">
+        <a-input v-model:value="formState.trademarkName" />
       </a-form-item>
-      <a-form-item name="description" label="Description">
-        <a-textarea v-model:value="formState.description" />
-      </a-form-item>
-      <a-form-item
-        name="modifier"
-        class="collection-create-form_last-form-item"
-      >
-        <a-radio-group v-model:value="formState.modifier">
-          <a-radio value="public">Public</a-radio>
-          <a-radio value="private">Private</a-radio>
-        </a-radio-group>
+      <a-form-item label="Logo" name="trademarkLogo">
+        <a-input v-model:value="formState.trademarkLogo" />
       </a-form-item>
     </a-form>
   </a-modal>
 </template>
 <script lang="ts" setup>
-import { reactive, ref, toRaw } from 'vue'
-import type { FormInstance } from 'ant-design-vue'
+import { reactive } from 'vue'
 
 // 定义属性
-defineProps(['visible', 'confirmLoading'])
-
-interface Values {
-  title: string
-  description: string
-  modifier: string
-}
-
-const formRef = ref<FormInstance>()
-
-const formState = reactive<Values>({
-  title: '',
-  description: '',
-  modifier: 'public',
+defineProps({
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+  confirmLoading: {
+    type: Boolean,
+    default: false,
+  },
 })
 
-const onOk = () => {
-  formRef.value
-    .validateFields()
-    .then((values) => {
-      console.log('Received values of form: ', values)
-      console.log('formState: ', toRaw(formState))
-      visible.value = false
-      formRef.value.resetFields()
-      console.log('reset formState: ', toRaw(formState))
-    })
-    .catch((info) => {
-      console.log('Validate Failed:', info)
-    })
+interface FormState {
+  trademarkName: string
+  trademarkLogo: object | null
 }
+
+const formState = reactive<FormState>({
+  trademarkName: '',
+  trademarkLogo: null,
+})
 </script>
