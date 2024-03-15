57f25f9cdfafa865444f8f317be9d3beaba1878c
cao-qh <caoqihao88@gmail.com>
2024-03-15 13:58:03 +0800

    定义品牌添加和修改方法

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,6 +1,6 @@
 // 书写品牌管理模块接口
 import request from '@/utils/request'
-import type { TradeMarkResponseData } from './type'
+import type { TradeMarkResponseData, TradeMark } from './type'
 // 品牌管理模块接口地址
 enum API {
   // 获取已有的品牌接口
@@ -11,7 +11,18 @@ enum API {
   UPDATETRADEMARK_URL = '/admin/product/baseTrademark/update',
 }
 
+// 获取品牌列表
 export const reqHasTrademark = (page: number, limit: number) =>
   request.get<any, TradeMarkResponseData>(
     API.TRADEMARK_URL + `${page}/${limit}`,
   )
+
+// 添加玉修改已有品牌接口方法
+export const reqAddOrUpdateTradeMark = (data: TradeMark) => {
+  // 修改已有品牌的数据
+  if (data.id) {
+    return request.put<any, any>(API.UPDATETRADEMARK_URL, data)
+  } else {
+    // 新增品牌
+  }
+}
