b70d28d0abb8f046d8c15b4a0999b4d4b5266c98
cao-qh <caoqihao88@gmail.com>
2024-04-08 17:51:37 +0800

    用户管理静态搭建

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -1,6 +1,22 @@
 <template>
   <PageWrapper>
-    <h1>用户管理</h1>
+    <a-form>
+      <a-row>
+        <a-col :span="6">
+          <a-form-item label="用户名">
+            <a-input placeholder="请输入用户名" />
+          </a-form-item>
+        </a-col>
+        <a-col :span="18">
+          <a-space style="float: right">
+            <a-button type="primary">查询</a-button>
+            <a-button>重置</a-button>
+          </a-space>
+        </a-col>
+      </a-row>
+    </a-form>
+
+    <a-table></a-table>
   </PageWrapper>
 </template>
 
