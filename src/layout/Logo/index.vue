87278641b36992a419cf51ad71e251ef8032ebe4
cao-qh <caoqihao88@gmail.com>
2024-04-16 23:42:06 +0800

    修改主题色完成，logo展示组件优化

--- src/layout/Logo/index.vue
+++ src/layout/Logo/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div v-if="!setting.logoHidden" class="logo">
     <img :src="setting.logo" alt="logo" />
-    <span :class="{ dark: !layoutSettingStore.dark }">{{ setting.title }}</span>
+    <span>{{ setting.title }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import setting from '@/setting'
 import useLayoutSettingStore from '@/store/modules/setting'
 const layoutSettingStore = useLayoutSettingStore()
@@ -13,25 +14,30 @@ const layoutSettingStore = useLayoutSettingStore()
 defineOptions({
   name: 'Logo',
 })
+
+const color = computed(() => {
+  return layoutSettingStore.dark ? '#d9d9d9' : layoutSettingStore.themeColor
+})
 </script>
 
 <style scoped lang="scss">
 .logo {
   height: $base-logo-height;
   line-height: calc($base-logo-height - 20px);
-  padding: 10px 16px;
-  color: #fff;
+  padding: 5px 16px;
   display: flex;
   justify-content: center;
+  align-items: center;
+
+  img {
+    height: 100%;
+  }
 
   span {
     margin-left: 8px;
     font-size: 16px;
     font-weight: bold;
-  }
-
-  span.dark {
-    color: #1890ff;
+    color: v-bind(color);
   }
 }
 </style>
--- src/layout/Side/index.vue
+++ src/layout/Side/index.vue
@@ -7,7 +7,7 @@
   >
     <a-form :wrapper-col="{ span: 6 }">
       <a-form-item label="主题颜色">
-        <a-input type="color" :value="layoutSettingStore.themeColor" />
+        <a-input type="color" v-model:value="color" @blur="changeThemeColor" />
       </a-form-item>
       <a-form-item label="暗黑模式">
         <a-switch v-model:checked="layoutSettingStore.dark"></a-switch>
@@ -17,8 +17,19 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import useLayoutSettingStore from '@/store/modules/setting'
+
+defineOptions({
+  name: 'Side',
+})
+
 const layoutSettingStore = useLayoutSettingStore()
+const color = ref(layoutSettingStore.themeColor)
+
+const changeThemeColor = () => {
+  layoutSettingStore.themeColor = color.value
+}
 </script>
 
 <style></style>
