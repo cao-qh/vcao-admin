493c9453663090b0eb19a88f38df8a13eca0de96
cao-qh <caoqihao88@gmail.com>
2024-04-17 15:06:45 +0800

    数据大屏顶部完成

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="screen" ref="screen">
-      <div class="top">顶部</div>
+      <Top />
       <div class="bottom">
         <div class="left">左侧</div>
         <div class="center">中间</div>
@@ -13,6 +13,7 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import Top from './components/Top/index.vue'
 
 defineOptions({ name: 'Screen' })
 const screen = ref()
@@ -40,10 +41,26 @@ function getScale(w = 1920, h = 1080) {
     position: fixed;
     width: 1920px;
     height: 1080px;
-    background: red;
     left: 50%;
     top: 50%;
     transform-origin: left top;
+
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .right {
+        flex: 1;
+      }
+      .left {
+        flex: 1;
+      }
+      .center {
+        flex: 2;
+      }
+    }
   }
 }
 </style>
