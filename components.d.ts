05cbcbde665554f3854fe321ffa59fe54f618fd4
cao-qh <caoqihao88@gmail.com>
2024-04-19 11:29:52 +0800

    添加活动搭建

--- components.d.ts
+++ components.d.ts
@@ -31,6 +31,8 @@ declare module 'vue' {
     AModal: typeof import('ant-design-vue/es')['Modal']
     APopconfirm: typeof import('ant-design-vue/es')['Popconfirm']
     ARow: typeof import('ant-design-vue/es')['Row']
+    ASelect: typeof import('ant-design-vue/es')['Select']
+    ASelectOption: typeof import('ant-design-vue/es')['SelectOption']
     ASpace: typeof import('ant-design-vue/es')['Space']
     ASwitch: typeof import('ant-design-vue/es')['Switch']
     ATable: typeof import('ant-design-vue/es')['Table']
--- src/views/admin/activity-manager/add-activity/index.vue
+++ src/views/admin/activity-manager/add-activity/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>添加活动</div>
+  <PageWrapper>
+    <SearchForm :searchLoading="searchLoading" :formItems="formItems" />
+  </PageWrapper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import SearchForm from '@/components/SearchForm/index.vue'
+import type { FormItems } from '@/components/SearchForm/type'
+
+const searchLoading = ref(false)
+const formItems = reactive<FormItems>([
+  {
+    filed: 'status',
+    label: '状态',
+    type: 'select',
+    placeholder: '请选择状态',
+    value: null,
+    options: [
+      { label: '全部', value: 0 },
+      { label: '启用', value: 1 },
+      { label: '禁用', value: 2 },
+    ],
+  },
+])
+</script>
 
 <style></style>
